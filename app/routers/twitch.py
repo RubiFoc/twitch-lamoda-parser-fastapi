835696e2f9d1f0ce3544ea49845c4b2425a6054c
RubiFoc <zhenya20057@mail.ru>
2024-08-13 14:59:41 +0300

    twitch category routers added

--- app/routers/twitch.py
+++ app/routers/twitch.py
@@ -1,8 +1,9 @@
 from fastapi import APIRouter, HTTPException
+from fastapi.encoders import jsonable_encoder
 
 from app.core.mongo import MongoService
 from app.core.twitch_parser import get_top_streams_by_query
-from app.models.twitch import Filter
+from app.models.twitch import Filter, Category
 from app.services.kafka import KafkaService
 
 router = APIRouter(prefix="/twitch")
@@ -18,5 +19,39 @@ async def start_parsing(filter: Filter):
 
 
 @router.post("/get_streamers_by_category")
-async def get_category_streamers(collection_name: str, query: str):
-    await mongo_service.get_documents(collection_name, query)
+async def get_top_category_streamers(category: str):
+    query = {"game_name": category}
+    streamers = mongo_service.get_documents('twitch_channels', query)
+    streamers_list = []
+    for streamer in streamers:
+        streamer['_id'] = str(streamer['_id'])
+        streamers_list.append(streamer)
+
+    streamers_list.sort(key=lambda x: x['viewers_count'], reverse=True)
+    response = [{**streamer} for streamer in streamers_list]
+    return jsonable_encoder({category: response})
+
+
+@router.post("/add_category")
+async def add_category(category: Category):
+    query = category.dict()
+    category_query = {"name": query["name"]}
+    id_query = {"id": category.id}
+    if mongo_service.db['twitch_categories'].count_documents(category_query) > 0:
+        return {"message": "The category already exists!"}
+    if mongo_service.db['twitch_categories'].count_documents(id_query) > 0:
+        return {"message": "The category with this id already exists!"}
+    mongo_service.insert_document('twitch_categories', query)
+    return {"message": "Added successfully!"}
+
+
+@router.post("/delete_category")
+async def delete_category(category: Category):
+    query = category.dict()
+    mongo_service.delete_document('twitch_categories', query)
+    return {"message": "Deleted successfully!"}
+
+
+@router.post("/update_category")
+async def update_category(category_to_update: Category, new_values: Category):
+    mongo_service.update_document('twitch_categories', category_to_update.dict(), new_values.dict())
